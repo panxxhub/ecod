2c1c28d108efa2f30025c6a7f8bcf52b7a1661a8
pan <panezion@outlook.com>
2024-08-16 08:14:43 +0800

    1c00

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
+#include <sdo/sdo_list.h>
+#include <stdio.h>
+#include <unistd.h>
+#define SDO_FILE_NAME "sdo_list.bin"
 int main() {
-    int a = 10;
-    int b = 20;
-    int c = a + b;
-    return 0;
+  // struct fs_file_t {
+  //    FILE* file_p
+  // };
+  struct fs_file_t my_file;
+
+  sdo_restore_defaults();
+
+  //  open file, create if not exist
+  //   my_file.file_p = fopen(SDO_FILE_NAME, "a+");
+  // check if the file exists first
+  bool file_exists = false;
+  if (access(SDO_FILE_NAME, F_OK) != -1) {
+    // file exists
+    file_exists = true;
+    my_file.file_p = fopen(SDO_FILE_NAME, "r+");
+  } else {
+    // file doesn't exist
+    my_file.file_p = fopen(SDO_FILE_NAME, "w+");
+  }
+
+  if (file_exists) {
+    sdo_objects_init_from_storage(&my_file);
+  } else {
+    sdo_objects_save_to_storage(&my_file);
+  }
+  return 0;
 }
