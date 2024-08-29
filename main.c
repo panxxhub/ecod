d006a92904808dd8646586f4d2f0ba59daa4ed77
pan <panezion@outlook.com>
2024-08-29 13:32:37 +0800

    add extern identifier

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <sdo/sdo_list.h>
 #include <stdio.h>
+#include <strings.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #define SDO_FILE_NAME "sdo_list.bin"
@@ -63,6 +64,14 @@ int main() {
   printf("Number of sm2: %d, 1st vel loop gain %d\n",
          sync_manager_channel_2.number_of_assigned_pdos,
          x_1st_velocity_loop_gain);
+
+  int ffs_0 = ffs(0);
+  int ffs_1 = ffs(1);
+  int ffs_2 = ffs(2);
+  int ffs_64 = ffs(64);
+
+  printf("ffs_0: %d, ffs_1: %d, ffs_2: %d, ffs_64: %d\n", ffs_0, ffs_1, ffs_2,
+         ffs_64);
 
   return 0;
 }
